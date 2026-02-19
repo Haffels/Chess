376200c5675f270b4ed7b3b889ccac1688a19e17
Harry <hrbloomfield@icloud.com>
2026-02-19 23:31:27 +0000

    fixes

---
+++ Chess-ALEVEL/Logic/Bot/CalculateMaterial.cs
@@ -0,0 +1,25 @@
+namespace Game.Logic.Bot
+{
+    public class CalculateMaterial
+    {
+        public static int GetTotalMaterial(Board board)
+        {
+            int total = 0;
+
+            for (int i = 0; i < 64; i++)
+            {
+                int piece = board.gameBoard[i];
+
+                if (piece != Pieces.NO_PIECE && Math.Abs(piece) != Pieces.EN_PASSANT_MARKER)
+                {
+                    int pieceType = Math.Abs(piece);
+
+                    if (Kenith.pieceValues.ContainsKey(pieceType))
+                        total += Kenith.pieceValues[pieceType];
+                }
+            }
+            
+            return total;
+        }
+    }
+}
--- Chess-ALEVEL/Logic/Bot/GetAdaptiveDepth.cs
+++ Chess-ALEVEL/Logic/Bot/GetAdaptiveDepth.cs
@@ -4,18 +4,17 @@ namespace Game.Logic.Bot
     {
         public static int GetDepth(Board board, int moveCount)
         {
-            int depth = Kenith.MAX_DEPTH;
+            int material = CalculateMaterial.GetTotalMaterial(board);
             
-            if (moveCount <= 10)
-                depth = 7;
-            else if (moveCount <= 15)
-                depth = 6;
-            else if (moveCount <= 20)
-                depth = 5;
-            else
-                depth = 4;
-
-            return depth;
+            if (moveCount <= 10) 
+                return 7;
+            else if (moveCount <= 20 && material <= 2500) 
+                return 6;
+            else if (moveCount <= 25) 
+                return 5;
+            else 
+                return 4;
+            
         }
     }
 }
